cdb682ba0a4201454a61565979ec204634cac01f
John Axel Eriksson <john@insane.se>
2013-10-29 10:00:36 +0100

    Fix tmux devenv

--- home/.profile.d/101_dev.sh
+++ home/.profile.d/101_dev.sh
@@ -2,29 +2,28 @@ devenv(){
   for project in $(echo -n $DEV_PROJECTS); do
     tmux ls | grep $project && continue
     tmux set-option default-path $DEVDIR/$project
-    tmux new-session -d -s $project
-    tmux new-window -t $project:0 -n 'Vim'
-    tmux select-window -t $project:0
-    tmux send-keys "cd $DEVDIR/$project; vim" 'C-m'
-    tmux new-window -t $project:1 -n 'Specs'
+    tmux new-session -d -s $project -n 'Vim'
     tmux select-window -t $project:1
+    tmux send-keys "cd $DEVDIR/$project; vim" 'C-m'
+    tmux new-window -t $project:2 -n 'Specs'
+    tmux select-window -t $project:2
+    tmux send-keys "cd $DEVDIR/$project" 'C-m'
     tmux send-keys 'bundle exec rspec'
-    tmux select-window -t $project:0
+    tmux select-window -t $project:1
   done
 
   if [ ! $(tmux ls | grep servers) ]; then
     cd $HOME
-    tmux new-session -d -s servers
-    tmux new-window -t servers:0
-    tmux select-window -t servers:0
+    tmux new-session -d -s servers -n 'Servers'
+    tmux select-window -t servers:1
     tmux send-keys "cd $HOME" 'C-m'
   fi
 
   if [ ! $(tmux ls | grep shell) ]; then
     cd $HOME
-    tmux new-session -d -s shell
-    tmux new-window -t shell:0
-    tmux select-window -t shell:0
+    tmux new-session -d -s shell -n 'Shell'
+    tmux new-window -t shell:1
+    tmux select-window -t shell:1
     tmux send-keys "cd $HOME" 'C-m'
   fi
 
