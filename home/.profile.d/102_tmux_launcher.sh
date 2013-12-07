67fe19a521d54898230282253a26b948a9f0a461
John Axel Eriksson <john@insane.se>
2013-12-07 20:41:18 +0100

    Hmm this is kind of hard

--- home/.profile.d/102_tmux_launcher.sh
+++ home/.profile.d/102_tmux_launcher.sh
@@ -3,10 +3,11 @@ if [[ "$OS" != "Darwin" ]]; then
   if [[ "$TERM" != "screen" ]]; then
     if [[ ! -z "$SSH_TTY" ]]; then
       echo "Inside ssh, auth sock is $SSH_AUTH_SOCK"
-      if [[ -z "$SSH_AUTH_SOCK" ]]; then
-        echo "Auth socket is $SSH_AUTH_SOCK, linking..."
-        rm "~/.ssh/.wrap_auth_sock"
-        ln -sf "$SSH_AUTH_SOCK" "~/.ssh/.wrap_auth_sock"
+      SOCK="/tmp/ssh-agent-$USER-tmux"
+      if test $SSH_AUTH_SOCK && [[ "$SSH_AUTH_SOCK" != "$SOCK" ]]; then
+        rm -f $SOCK
+        ln -sf $SSH_AUTH_SOCK $SOCK
+        export SSH_AUTH_SOCK=$SOCK
       fi
       if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
         `ssh-agent -a $SSH_AUTH_SOCK` > /dev/null >&1
@@ -15,8 +16,6 @@ if [[ "$OS" != "Darwin" ]]; then
       if [[ -z $SSH_AGENT_PID ]]; then
         export SSH_AGENT_PID=`cat ~/.ssh/.auth_pid`
       fi
-      ssh-add 2>/dev/null
-      export SSH_AUTH_SOCK="~/.ssh/.wrap_auth_sock"
     fi
     WHOAMI=$(whoami)
     if tmux has-session -t $WHOAMI 2>/dev/null; then
