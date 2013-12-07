9b9bc2b306a2a37489b611859df773a5104806b9
John Axel Eriksson <john@insane.se>
2013-12-07 21:04:20 +0100

    Remove junk

--- home/.profile.d/102_tmux_launcher.sh
+++ home/.profile.d/102_tmux_launcher.sh
@@ -2,19 +2,11 @@ OS=`uname`
 if [[ "$OS" != "Darwin" ]]; then
   if [[ "$TERM" != "screen" ]]; then
     if [[ ! -z "$SSH_TTY" ]]; then
-      echo "Inside ssh, auth sock is $SSH_AUTH_SOCK"
       SOCK="/tmp/ssh-agent-$USER-tmux"
       if test $SSH_AUTH_SOCK && [[ "$SSH_AUTH_SOCK" != "$SOCK" ]]; then
         rm -f $SOCK
         ln -sf $SSH_AUTH_SOCK $SOCK
         export SSH_AUTH_SOCK=$SOCK
-      fi
-      if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
-        `ssh-agent -a $SSH_AUTH_SOCK` > /dev/null >&1
-        echo $SSH_AGENT_PID > ~/.ssh/.auth_pid
-      fi
-      if [[ -z $SSH_AGENT_PID ]]; then
-        export SSH_AGENT_PID=`cat ~/.ssh/.auth_pid`
       fi
     fi
     WHOAMI=$(whoami)
