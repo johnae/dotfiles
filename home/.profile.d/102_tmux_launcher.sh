56bdc4b589b95ce480ef9d963ebf7de8557b9725
John Axel Eriksson <john@insane.se>
2013-12-07 20:26:28 +0100

    More debug

--- home/.profile.d/102_tmux_launcher.sh
+++ home/.profile.d/102_tmux_launcher.sh
@@ -2,12 +2,13 @@ OS=`uname`
 if [[ "$OS" != "Darwin" ]]; then
   if [[ "$TERM" != "screen" ]]; then
     if [[ ! -z "$SSH_TTY" ]]; then
+      echo "Inside ssh, auth sock is $SSH_AUTH_SOCK"
       if [[ -z "$SSH_AUTH_SOCK" ]]; then
         echo "Auth socket is $SSH_AUTH_SOCK, linking..."
-        rm "$HOME/.ssh/.wrap_auth_sock"
-        ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/.wrap_auth_sock"
+        rm "~/.ssh/.wrap_auth_sock"
+        ln -sf "$SSH_AUTH_SOCK" "~/.ssh/.wrap_auth_sock"
       fi
-      export SSH_AUTH_SOCK="$HOME/.ssh/.wrap_auth_sock"
+      export SSH_AUTH_SOCK="~/.ssh/.wrap_auth_sock"
     fi
     WHOAMI=$(whoami)
     if tmux has-session -t $WHOAMI 2>/dev/null; then
