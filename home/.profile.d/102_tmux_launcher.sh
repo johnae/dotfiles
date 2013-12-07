80440a2d386e2b7cf4f8e6250128cf02c1b79e81
John Axel Eriksson <john@insane.se>
2013-12-07 21:13:48 +0100

    Disconnect ssh on detach from tmux

--- home/.profile.d/102_tmux_launcher.sh
+++ home/.profile.d/102_tmux_launcher.sh
@@ -12,9 +12,9 @@ if [[ "$OS" != "Darwin" ]]; then
     WHOAMI=$(whoami)
     if [[ $SHLVL != "2" ]]; then
       if tmux has-session -t $WHOAMI 2>/dev/null; then
-        tmux -2 attach-session -t $WHOAMI
+        exec tmux -2 attach-session -t $WHOAMI
       else
-        tmux -2 new-session -s $WHOAMI
+        exec tmux -2 new-session -s $WHOAMI
       fi
     fi
   fi
