1514396a6f8a63bbbcd61f85a5e6d5bc2d937059
John Axel Eriksson <john@insane.se>
2015-05-22 14:23:57 +0200

    Skip check for SSH_TTY when loading ssh keys

--- home/.profile.d/102_tmux_launcher.sh
+++ home/.profile.d/102_tmux_launcher.sh
@@ -1,14 +1,14 @@
 OS=`uname`
 if [[ "$OS" != "Darwin" ]]; then
   if [[ "$TERM" != "screen" ]]; then
-    if [[ ! -z "$SSH_TTY" ]]; then
+    #if [[ ! -z "$SSH_TTY" ]]; then
       SOCK="/tmp/ssh-agent-$USER-tmux"
       if test $SSH_AUTH_SOCK && [[ "$SSH_AUTH_SOCK" != "$SOCK" ]]; then
         rm -f $SOCK
         ln -sf $SSH_AUTH_SOCK $SOCK
         export SSH_AUTH_SOCK=$SOCK
       fi
-    fi
+    #fi
     if [[ $SHLVL != "2" ]]; then
       if tmux has-session 2>/dev/null; then
         #exec tmux -2 attach-session -t $WHOAMI
